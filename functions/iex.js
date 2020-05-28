6f11c3bb201e870fc90b5fe221aa6d44f8cf9e1f
Van Zelleb <wannikid@gmail.com>
2020-05-29 01:38:32 +0200

    updated iex.js

--- functions/iex.js
+++ functions/iex.js
@@ -1,12 +1,14 @@
 const callApi = require("./utils/callApi");
 
-exports.handler = async (event, context) => {
+exports.handler = async event => {
   const token = process.env.VUE_APP_IEXCLOUD_SECRET_KEY;
   // Parse the body contents into an object.
-  let { uri, params } = JSON.parse(event.body);
-  uri = new URL(uri);
-  params.token = token;
-  Object.keys(params).forEach(key => uri.searchParams.append(key, params[key]));
+  let body = JSON.parse(event.body);
+  let uri = new URL(body.uri);
+  body.params.token = token;
+  Object.keys(body.params).forEach(key =>
+    uri.searchParams.append(key, body.params[key])
+  );
   let response = await callApi(uri);
   return {
     statusCode: 200,
