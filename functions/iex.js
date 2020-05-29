ef5266df9f13086f37828dbf33388b245a4f5dce
Van Zelleb <wannikid@gmail.com>
2020-05-29 10:26:27 +0200

    api test

--- functions/iex.js
+++ functions/iex.js
@@ -1,7 +1,9 @@
 const callApi = require("./utils/callApi");
 
 const headers = {
-  "Access-Control-Allow-Origin": "*"
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept"
 };
 
 exports.handler = async event => {
